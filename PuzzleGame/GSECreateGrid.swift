98c21231ad410d2cf01f19d65c8f5b99c0ced11b
sans-connaissance <davemalicke@gmail.com>
2021-08-08 16:57:37 -0400

    Finished adding create level and wrong/right answer logic

--- PuzzleGame/GSECreateGrid.swift
+++ PuzzleGame/GSECreateGrid.swift
@@ -12,15 +12,13 @@ extension GameScene {
     
     
     func createGrid() {
-        
+
         let xOffset = -440
         let yOffset = -280
-        var imageNumber = 0
-        
+
         for row in 0 ..< 8 {
-            imageNumber = imageNumber + 1
             for col in 0 ..< 12 {
-                let item = SKSpriteNode(imageNamed: "\(imageNumber)")
+                let item = SKSpriteNode(imageNamed: "1")
                 item.position = CGPoint(x: xOffset + (col * 80), y: yOffset + (row * 80))
                 addChild(item)
             }
@@ -31,7 +29,7 @@ extension GameScene {
         var itemsToShow = 4 + (level * 4)
         
         // find all nodes that belong to our scene that are not called "Background"
-        
+        //This is the trick!!
         let items = children.filter{$0.name != "background"}
         
         //Shuffle those nodes so they are in a random order
@@ -42,6 +40,34 @@ extension GameScene {
             //and hide each one
             item.alpha = 0
         }
+        
+       // figure out the hightest number we're going to show
+        let highest = Int.random(in: 5...15)
+        var others = [Int]()
+        
+        // generate lots of numbers lower than that
+        for _ in 1 ..< itemsToShow {
+            let num = Int.random(in: 0 ..< highest)
+            others.append(num)
+        }
+        
+        for (index, number) in others.enumerated() {
+            //pull out one of the random balls
+            let item = shuffled[index]
+            
+            //give it the correct texture for its new number
+            item.texture = SKTexture(imageNamed: String(number))
+            
+            // show it immediately
+            item.alpha = 1
+            item.name = "wrong"
+
+        }
+        
+        shuffled.last?.texture = SKTexture(imageNamed: String(highest))
+        shuffled.last?.alpha = 1
+        shuffled.last?.name = "correct"
+        
     }
     
 }
