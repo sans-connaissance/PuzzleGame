b11755e35b9fd57f4911b3330f623a8ef6de03e4
sans-connaissance <davemalicke@gmail.com>
2021-08-09 18:27:30 -0400

    Added score keeping

--- PuzzleGame/GameScene.swift
+++ PuzzleGame/GameScene.swift
@@ -11,7 +11,12 @@ import GameplayKit
 class GameScene: SKScene {
     
     var level = 1
-    
+    let scoreLabel = SKLabelNode(fontNamed: "Optima-ExtraBlack")
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "SCORE: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background-pattern")
@@ -19,8 +24,16 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
+        scoreLabel.position = CGPoint(x: -480, y: 330)
+        scoreLabel.horizontalAlignmentMode = .left
+        
+        scoreLabel.zPosition = 1
+        background.addChild(scoreLabel)
+        
         createGrid()
         createLevel()
+        
+        score = 0
 
     }
     
@@ -40,8 +53,10 @@ class GameScene: SKScene {
         //Pretty cool how you are using the assigned names to drive logic and behavior.
         if tapped.name == "correct" {
             correctAnswer(node: tapped)
+            score += 1
         } else if tapped.name == "wrong" {
             wrongAnswer(node: tapped)
+            score -= 1
         }
         
         
